606dca1cf6c71b18cd957718e5d836890a8153b8
Wes Oldenbeuving <narnach@gmail.com>
2009-10-01 15:14:14 +0200

    Revert "Load the haml template directly instead of using the haml helper"
    This reverts commit 9075a0bb0c9898ff3ad321e259ac950b01b6135b.

--- rubyist.rb
+++ rubyist.rb
@@ -4,9 +4,5 @@ require 'haml'
 set :views, File.join(File.dirname(__FILE__), 'haml')
 
 get '/' do
-  template('gmaps').render
-end
-
-def template(name)
-  Haml::Engine.new(File.read(File.join(File.dirname(__FILE__),'haml',"#{name}.haml")))
+  haml :gmaps
 end

225412309b33cd1272e07360e2fb8417c8e4ea02
Fernando Vezzosi <fernando.vezzosi@booking.com>
2009-10-01 21:01:53 +0200

    pseudo-working openid login

--- rubyist.rb
+++ rubyist.rb
@@ -4,28 +4,59 @@ require 'openid'
 require 'openid/store/filesystem'
 
 set :views, File.join(File.dirname(__FILE__), 'haml')
+set :sessions, true
 
 get '/' do
   haml :gmaps
 end
 
 get '/login' do
+  if session[:openid]
+    return 'You are already logged in as '+session[:openid]+', you might want to <a href="/logout">Log Out</a>'
+  end
+
   haml :login
 end
 
-get '/login/complete' do
-  "completed"
-end
-
 post '/login' do
-  oid = OpenID::Consumer.new(
+  session[:oid] = OpenID::Consumer.new(
     session,
     OpenID::Store::Filesystem.new( '/tmp/openid' )
   )
-  oid_response = oid.begin params['openid_url']
+  oid_response = session[:oid].begin params['openid_url']
   if oid_response
-    redirect oid_response.redirect_url('http://localhost:9393/', 'http://localhost:9393/login/complete')
+    redirect oid_response.redirect_url(request.url, request.url + '/complete')
   else
     "failure"
+  end
+end
+
+get '/login/complete' do
+  if session[:openid]
+    return 'wat'
+  end
+
+  oid_response = session[:oid].complete( params, request.script_name + '/login/complete')
+  if session[:openid] = oid_response.identity_url
+    'You have successfully logged in as ' + session[:openid]
+  else
+    'please <a href="/login">Log In</a>'
+  end
+end
+
+get '/logout' do
+  if session[:openid]
+    session.delete :openid
+    'You have been logged out'
+  else
+    'You are already logged out'
+  end
+end
+
+get '/check' do
+  if session[:openid]
+    "you are logged in"
+  else
+    "please log in"
   end
 end

eed132dce817e13d30afdac06d44036e1f973b19
Julio Javier Cicchelli <me@javiercicchelli.info>
2009-10-10 04:54:38 +0200

    Rearranged the DataMapper code and defined the Country mapping class.

---
+++ lib/rubyists/model.rb
@@ -0,0 +1,94 @@
+require 'dm-core'
+require 'dm-validations'
+require 'dm-aggregates'
+
+DataMapper.setup :default, (ENV['DATABASE_URL'] || "postgres://postgres:postgres@localhost/rubyists")
+
+class Country
+  include DataMapper::Resource
+  
+  property :code, String, :key => true, :length => 2
+  property :name, String
+
+  validates_is_unique :code
+  
+  class << self
+    def populate
+      File.open("#{Dir.pwd}/data/countries.csv", File::RDONLY).each_line do |row|
+        country = Country.new
+
+        country.name = ""
+        name, code = row.split(';')
+        name.downcase!.split(' ').each{|fraction| country.name << "#{fraction.capitalize} "}
+        country.name.chop!
+        country.code = code[0, 2]
+
+        country.save
+      end
+    end
+  end
+end
+
+#class Member
+#  include DataMapper::Resource
+#
+#  property :id, Serial
+#  property :openid, String
+#  property :name, String
+#  property :city, String
+#  property :country, String
+#
+#  def self.seed
+#    return if count > 0
+#    member = new
+#
+#    member.openid = "Hola"
+#    member.name = "Javier Cicchelli"
+#    member.city = "Buenos Aires"
+#    member.country = "Argentina"
+#
+#    member.save
+#  end
+#end
+#
+#class UserGroup
+#  include DataMapper::Resource
+#
+#  property :id, Serial
+#  property :name, String
+#  property :city, String
+#  property :country, String
+#
+#  def self.seed
+#    {
+#      'Amsterdam.rb' => {:city => 'Amsterdam', :country => 'The Netherlands'},
+#      'Utrecht.rb' => {:city => 'Utrecht', :country => 'The Netherlands'},
+#      'Den Haag.rb' => {:city => 'Den Haag', :country => 'The Netherlands'},
+#    }.each do |name, attrs|
+#      next if count(:name => name) > 0
+#      ug = new
+#
+#      ug.name = name
+#      ug.city = attrs[:city]
+#      ug.country = attrs[:country]
+#
+#      ug.save
+#    end
+#  end
+#
+#  def to_json
+#    attributes.to_json
+#  end
+#end
+#
+#class DataMapper::Collection
+#  def to_json
+#    to_a.to_json
+#  end
+#end
+
+DataMapper.auto_migrate!
+
+Country.populate
+#Member.seed
+#UserGroup.seed

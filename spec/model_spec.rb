10a3ad72e6a8cfbd9fa8472260d7a538e4e9e516
Julio Javier Cicchelli <me@javiercicchelli.info>
2009-10-11 16:07:38 +0200

    Refactored the fail test cases for the Country mapping class.

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -29,74 +29,34 @@ describe Country do
       saved_country.name.should == country.name
     end
     
-    it "should not save a new country with a defined 2-letter uppercased code and a nil name." do
-      country = Country.new(:code => 'XX')
-      
-      country.should be_an_instance_of(Country)
-      country.code.should be_an_instance_of(String)
-      country.code.should =~ /^[A-Z]{2}$/
-      country.code.length.should == 2
-      country.name.should be_nil
-      
-      country.save
-      
-      Country.get(country.code).should be_nil
+    it "should not save a new country in any other case." do
+      countries = [{:code => 'X', :name => 'Test Country'}, {:code => 'Xx', :name => 'Test Country'}, 
+                   {:code => 'X0', :name => 'Test Country'}, {:code => 'XXX', :name => 'Test Country'}, 
+                   {:code => nil, :name => 'Test Country'}, {:code => 'XX', :name => nil}]
+                   
+      countries.each do |country|
+        test_country = Country.new(:code => country[:code], :name => country[:name])
+        
+        test_country.should be_an_instance_of(Country)
+        
+        unless country[:code].nil?
+          test_country.code.should be_an_instance_of(String)
+           country[:code].length == 2 ? test_country.code.length.should == 2 : 
+                                        test_country.code.length.should_not == 2
+           country[:code].match(/^[A-Z]{2}$/).nil? ? test_country.code.should_not =~ /^[A-Z]{2}$/ :
+                                                     test_country.code.should =~ /^[A-Z]{2}$/
+        end
+        
+        unless country[:name].nil?
+          test_country.name.should be_an_instance_of(String)
+        end
+        
+        test_country.save
+        
+        Country.get(test_country.code).should be_nil
+      end
     end
-    
-    it "should not save a new country with a 2-letter lowercased code and a name." do
-      country = Country.new(:code => 'xx', :name => 'Test Country')
-      
-      country.should be_an_instance_of(Country)
-      country.code.should be_an_instance_of(String)
-      country.code.should_not =~ /^[A-Z]{2}$/
-      country.code.length.should == 2
-      country.name.should be_an_instance_of(String)
-      
-      country.save
-      
-      Country.get(country.code).should be_nil
-    end
-    
-    it "should not save a new country with a 2-letter mixed code and a name." do
-      country = Country.new(:code => 'Xx', :name => 'Test Country')
-      
-      country.should be_an_instance_of(Country)
-      country.code.should be_an_instance_of(String)
-      country.code.should_not =~ /^[A-Z]{2}$/
-      country.code.length.should == 2
-      country.name.should be_an_instance_of(String)
-      
-      country.save
-      
-      Country.get(country.code).should be_nil
-    end
-    
-    it "should not save a new country with an alphanumeric code and a name." do
-      country = Country.new(:code => 'X0', :name => 'Test Country')
-      
-      country.should be_an_instance_of(Country)
-      country.code.should be_an_instance_of(String)
-      country.code.should_not =~ /^[A-Z]{2}$/
-      country.code.length.should == 2
-      country.name.should be_an_instance_of(String)
-      
-      country.save
-      
-      Country.get(country.code).should be_nil
-    end
-    
-    it "should not save a new country with a nil code and a name." do
-      country = Country.new(:name => 'Test Country')
-      
-      country.should be_an_instance_of(Country)
-      country.code.should be_nil
-      country.name.should be_an_instance_of(String)
-      
-      country.save
-      
-      Country.get(country.code).should be_nil
-    end
-    
+     
     it "should populate its possible values from a comma-separated file." do
       Country.populate
       Country.all.size.should > 0

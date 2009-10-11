0e76cd3e542cf329a7290e994767e07b28adc2e9
Julio Javier Cicchelli <me@javiercicchelli.info>
2009-10-12 01:33:43 +0200

    Added the User mapping class.

--- lib/rubyists/model.rb
+++ lib/rubyists/model.rb
@@ -7,6 +7,8 @@ DataMapper.setup :default, (ENV['DATABASE_URL'] || "postgres://postgres:postgres
 class Country
   include DataMapper::Resource
   
+  has 1, :user
+  
   property :code, String, :length => 2, :format => /^[A-Z]{2}$/, :key => true
   property :name, String, :nullable => false
 
@@ -24,6 +26,22 @@ class Country
         country.save
       end
     end
+  end
+end
+
+class User
+  include DataMapper::Resource
+  
+  belongs_to :country
+  
+  property :id, Serial
+  property :openid, String, :nullable => false
+  property :name, String, :nullable => false, :format => /^([A-Z][a-z\.\-]*\s?)+$/
+  property :email, String, :nullable => false, :format => /^(?:[a-z]+)(\.[\w\-]+)*@([\w\-]+)(\.[\w\-\.]+)*(\.[a-z]{2,4})$/i   
+  property :city, String, :nullable => false, :format => /^([A-Z][a-z\.\-]*\s?)+$/
+  
+  before :save do
+    throw :halt if Country.get(country_code).nil?
   end
 end
 
